f187cedaded120d4abf62d736fbbddab46df5964
Junior Felix <juniorfelix8825@outlook.com>
2020-11-01 20:52:28 +0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Index: React.FC = () => {
+  return (
+    <>
+      <Title>My First Next Page</Title>
+    </>
+  );
+};
+const Title = styled.h1`
+  color: red;
+`;
+
+export default Index;
